3b0b08be7d0dfe0425b37fae400f01bf38f721c8
osman-butt <butt.osman@gmail.com>
2025-08-28 15:12:54 +0200

    Init commit

---
+++ src/main/java/ek/osnb/jpa/SpringJpaRelationsApplication.java
@@ -0,0 +1,13 @@
+package ek.osnb.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaRelationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ek/osnb/jpa/SpringJpaRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.osnb.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
